4f6cd3205ed262e3014764bf7315f6ce6ca5d5fe
hsalas006 <hsalas006@github.com>
2020-10-02 19:01:00 -0600

    auth process register and login

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Login from "../auth/Login";
+import Register from "../auth/Register";
 
 const Navbar = ({ title, icon }) => {
   return (
@@ -14,6 +16,12 @@ const Navbar = ({ title, icon }) => {
         </li>
         <li>
           <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </div>
